b23f927118a04ec7c454601411493d4d26c5b5a4
matteo <matteo99moro@gmail.com>
2018-03-08 18:46:58 +0100

    Insert Project

---
+++ Grafi/elements/arco.php
@@ -0,0 +1,18 @@
+<?php 
+
+class Arco
+{
+    function __construct($nodoa , $nodob , $durata)
+    {
+        $this->nodoa = $nodoa;
+        $this->nodob = $nodob;
+        $this->durata = $durata;
+    }
+
+    public function GetPeso()
+    {
+        return $this->durata;
+    }
+}
+
+?>
